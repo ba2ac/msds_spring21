d2e1978e7c5b49636660a7f0ce93935c0a831d43
brago513 <ba2ac@virginia.edu>
2021-01-18 16:36:57 -0500

    completed module 6

--- techbootcamp.R
+++ techbootcamp.R
@@ -38,7 +38,7 @@ is.word()
 # exercise 5: record and print a list
 make_list <- function(){
   mylist <- c()
-  size <- readline(prompt= "How long is the list? ")
+  size <- as.numeric(readline(prompt= "How long is the list? "))
   
   for(i in 1:size){
     newword <- readline(prompt= "Enter a new word: ")
@@ -48,3 +48,28 @@ make_list <- function(){
 }
 
 make_list()
+
+
+## module 6: csv + visualization + pipeline
+
+#exercise 1: recreate the example pipeline
+
+library(tidyverse)
+
+df <- read_csv("/Users/Brago/msds_spring21/andre.csv", col_names = TRUE)
+
+df <- filter(df, Year != 1976)
+df <- filter(df, Year <  1994)
+
+ggplot(data=df, aes(df$H)) + geom_histogram()
+
+# exercise 2: create my own pipeline
+
+# load the data
+df <- read_csv("/Users/Brago/msds_spring21/top100.csv", col_names = TRUE)
+
+# filter the guards
+df1 = filter(df, POSITION == 'Guard')
+
+# histogram of points
+ggplot(df1, aes(x= PTS)) + geom_histogram()
